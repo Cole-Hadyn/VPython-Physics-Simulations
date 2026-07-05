f1349c34503450c4e3cb7266daa41a86df1efc51
Hadyn Cole <hadyn173@gmail.com>
2026-07-05 16:39:23 -0500

    Working to make the simulation follow more realistic physical condition.

--- drone_package_catch.py
+++ drone_package_catch.py
@@ -1,3 +1,4 @@
+
 import os
 os.environ['VPYTHON_LAUNCH_BROWSER'] = 'False'
 
@@ -9,43 +10,60 @@ grass = box(pos = vec(75,-10,0), size = vec(400,3,70), color=color.green)
 pack = sphere(pos=vec(50,100,0), radius=3, color=color.red, make_trail = True)
 drone = sphere(pos=vec(0,0,0), radius=5, color = color.cyan, make_trail = True)
 
-pack.v = vec(2, -1, 0)
+pack.v = vec(2, -1, 0)          # Package starting velocity
+pack.m = 1.0
+pack.p = pack.m * pack.v
 
-drone.m = 2.0        # drone mass
-drone.p = vec(0, 0, 0)
-drone.pos = vec(0, 0, 0)
+drone.m = 2.0                   # drone mass
+drone.p = vec(0, 0, 0)          # Starting Momentum
+drone.pos = vec(0, 0, 0)        # Starting position
 
 t = 0
 deltat = 0.05
 
-scene.pause()
+scene.pause()                   # Pauses the simulation so that user can start when ready
 
-
+# Creating setup for graph that will track how far drone is from the package
 graph=gcurve(color=color.purple)
 
-
+g = vec(0, -9.8, 0)
 
 while pack.pos.y > grass.pos.y:
   rate(300)
   t = t + deltat
-  r=(pack.pos-drone.pos)
 
-  rhat=hat(r)
-  magr=mag(r)
-  Fnet = vec(150,100,0)
-  magFnet=mag(Fnet)
-  pack.pos = pack.pos + pack.v*deltat
-  drone.p = drone.p + (Fnet*deltat)
- 
-  drone.pos = drone.pos + (drone.p/drone.m) * deltat
-  graph.plot(t,magr)
+  Fg_drone = drone.m * g
+
+# Finding the vector 'r' that points from the drone to the package
+  r = (pack.pos - drone.pos)      # Updates 'r' each run through loop
+  rhat = hat(r)
+  magr = mag(r)
+
+# Regulates the drone speed as it approaches the package
+  if magr > 10:
+      magF_thrust = 40
+
+  else:
+      magF_thrust = 40 * (magr / 10)  # Force approaches 0 as magnitude of 'r' approaches 0
+
+  F_thrust = magF_thrust * rhat
+
+  Fnet_pack = vec(0, 0, 0)
+
+  Fnet_drone = F_thrust + Fg_drone # Steering thrust AND gravity pulling it down
   
-  # stop and exit from loop if drone goes too fast
-  if mag(drone.p/drone.m) > 9:
-    print("The drone exceeded its top speed.")
-    break
-  # stop and exit from loop if drone goes too fast
-  if mag(pack.pos-drone.pos) < 3:
+# 4. MOMENTUM UPDATES
+  pack.p = pack.p + (Fnet_pack * deltat)
+  drone.p = drone.p + (Fnet_drone * deltat)
+
+# 5. POSITION UPDATES
+  pack.pos = pack.pos + (pack.p / pack.m) * deltat
+  drone.pos = drone.pos + (drone.p / drone.m) * deltat
+
+  graph.plot(t, magr)   # Graph plots how far drone is from package 'r' over time 't'
+
+# Stop and exit from loop if drone goes too fast
+  if mag(pack.pos - drone.pos) < 3:
     print(f"The drone caught the package in {t - deltat: .2f} seconds.")
     break
 
