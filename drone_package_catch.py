37fb3d4dad0847da65c9f9f0761d1ecbd04eb748
Cole-Hadyn <hadyn173@gmail.com>
2026-07-06 19:27:02 -0500

    Implement reset function and button for simulation
    Added a reset function to restore simulation state and UI button for resetting the simulation.

--- drone_package_catch.py
+++ drone_package_catch.py
@@ -1,7 +1,6 @@
 
 import os
 os.environ['VPYTHON_LAUNCH_BROWSER'] = 'False'
-
 from vpython import *
 
 scene.center = vec(75,50,0)  # move camera to center the scene
@@ -22,33 +21,51 @@ t = 0
 deltat = 0.02
 
 scene.pause()                   # Pauses the simulation so that user can start when ready
-
 # Creating setup for graph that will track how far drone is from the package
 graph=gcurve(color=color.purple)
 
 g = vec(0, -9.8, 0)
 b = 1.5
+# CLEANED RESET FUNCTION: Restores both positions, velocities, trails, and clock state
+def reset_simulation():
+    global t, running
+    running = False    # Stops running physics calculations
+    t = 0
+    # Reset obs_1 back to initial state
+    pack.pos = vec(30, 200, 0)
+    pack.p = pack.m * vec(30, -1, 0)
+    pack.clear_trail()
+    
+    # Reset obs_2 back to initial state
+    drone.pos = vec(-30, 0, 0)
+    drone.p = vec(0, 0, 0)
+    drone.clear_trail()
+  
+    graph.delete()    # CLears old graph data
+    running = True
+
+# Create UI Button bound to the reset handler
+button(text="Repeat Simulation", bind=reset_simulation)
+scene.append_to_caption("\n\n")
+
+reset_simulation()
 
 while pack.pos.y > grass.pos.y:
   rate(300)
-
   t = t + deltat
 
   Fg_drone = drone.m * g
   Fg_pack = pack.m *g
-
 # Finding the vector 'r' that points from the drone to the package
   r = (pack.pos - drone.pos)      # Updates 'r' each run through loop
   rhat = hat(r)
   magr = mag(r)
-
 # Regulates the drone speed as it approaches the package
   if magr > 20:
       magF_thrust = 250
-
   else:
       magF_thrust = 250 * (magr / 20)  # Force approaches 0 as magnitude of 'r' approaches 0
-
+    
   F_thrust = magF_thrust * rhat
 
   drone_v = drone.p / drone.m
@@ -60,14 +77,11 @@ while pack.pos.y > grass.pos.y:
 #  MOMENTUM & POSITION UPDATES
   pack.p = pack.p + (Fnet_pack * deltat)
   pack.pos = pack.pos + (pack.p / pack.m) * deltat
-
+  
   drone.p = drone.p + (Fnet_drone * deltat)
   drone.pos = drone.pos + (drone.p / drone.m) * deltat
 
-
-
   graph.plot(t, magr)   # Graph plots how far drone is from package 'r' over time 't'
-
 # Stop and exit from loop if drone goes too fast
   if mag(pack.pos - drone.pos) < 4:
     print(f"The drone caught the package in {t - deltat: .2f} seconds.")
