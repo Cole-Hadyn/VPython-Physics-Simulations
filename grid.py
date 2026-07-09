c102a5dae28f1b38aded5d35ea92c5d2418b3f43
Hadyn Cole <hadyn173@gmail.com>
2026-07-09 17:46:51 -0500

    Working to create a cube composed of squares.

--- grid.py
+++ grid.py
@@ -1,19 +1,25 @@
+import os
+os.environ['VPYTHON_LAUNCH_BROWSER'] = 'False'
 
+from vpython import *
 
-
-GlowScript 2.9 VPython
 dx = 2
 y = -9
 dy = 2
-
+dz = 2
 
 
 while y < 11:	# Initail loop to set bounds for 'y'; loops until all rows are complete
   x = -9	# Establishing the starting conditions for the nested loop
 
   while x < 11:		# loop creates a row of boxes
-      rate(10)
-      box(pos=vec(x,y,0), color=color.green)
-      x = x + dx
+    rate(10)
+    z = 0
+    box(pos = vec(x,y,z), color = color.green)
+    x = x + dx
+
+    while z < 21:
+      box(pos = vec(x,y,z), color = color.green)
+      z = z + dz
 
   y = y + dy	# Updates 'y' position for the next row
